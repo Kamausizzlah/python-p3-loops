6b0c62d284ce8d6512238ce9a267c1d91790af8e
Ian Kamau <iankamau112@gmail.com>
2024-05-26 09:45:05 +0300

    initial commit

--- lib/looping.py
+++ lib/looping.py
@@ -3,11 +3,35 @@
 def happy_new_year():
     # code goes here!
     pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+        
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
     # code goes here!
     pass
+    squared_integers = [int * int for int in int_list]
+    return squared_integers
+
+square_integers([1, 2, 3, 4, 5])
 
 def fizzbuzz():
     # code goes here!
     pass
+    i = 1
+    for i in range(100):
+        i += 1
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+    
+fizzbuzz()
